385603cbd8b37cba3332c083b39217bc34cff7f5
yurik256 <yurik256@gmail.com>
2019-01-31 15:55:11 -0500

    add solution for valid anagram #242 problem

---
+++ csharp/Strings/ValidAnagram-242/ValidAnagram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Solutions.Strings
+{
+	public class ValidAnagramSolution
+	{
+		public bool IsAnagram(string s, string t)
+		{
+			if (s == null || t == null || s.Length != t.Length)
+			{
+				return false;
+			}
+
+			Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+
+			for (var i = 0; i < s.Length; i++)
+			{
+				char key = Char.ToLower(s[i]);
+				if (lettersCount.ContainsKey(s[i]))
+				{
+					lettersCount[key] += 1;
+				}
+				else
+				{
+					lettersCount.Add(key, 1);
+				}
+			}
+
+			for (var i = 0; i < t.Length; i++)
+			{
+				char key = Char.ToLower(t[i]);
+
+				if (lettersCount.ContainsKey(key))
+				{
+					var characterCount = lettersCount[key];
+					if (characterCount == 1)
+					{
+						lettersCount.Remove(key);
+					}
+					else
+					{
+						lettersCount[key] -= 1;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ csharp/Strings/ValidAnagram-242/ValidAnagramTest.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace Solutions.Strings
+{
+	public class ValidAnagramTest
+	{
+		private readonly ValidAnagramSolution _validAnagramSolution;
+		public ValidAnagramTest()
+		{
+			_validAnagramSolution = new ValidAnagramSolution();
+		}
+
+		[Theory]
+		[InlineData("123", "12")]
+		[InlineData("12", "123")]
+		[InlineData("12", "")]
+		[InlineData("", "123")]
+		[InlineData(null, "123")]
+		[InlineData("123", null)]
+		public void ShouldReturnFalseWhenStringsHaveDifferentLength(string t, string s)
+		{
+			var result = _validAnagramSolution.IsAnagram(t, s);
+
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ShouldReturnFalseWhenStringsHaveSameLengthButAreNotAnagram()
+		{
+			var result = _validAnagramSolution.IsAnagram("anagram", "mnagram");
+
+			Assert.False(result);
+		}
+
+		[Theory]
+		[InlineData("anagram", "nagaram")]
+		[InlineData("aNaGram", "Nagaram")]
+		public void ShouldReturnTrueWhenStringsAreAnagrams(string s, string t)
+		{
+			var result = _validAnagramSolution.IsAnagram(s, t);
+
+			Assert.True(result);
+		}
+	}
+}
